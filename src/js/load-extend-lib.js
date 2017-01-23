2aed2f6230eb533ea2c87e81f0220f6c828f9b61
bachlx-bitmark <bachlx@bitmark.com>
2017-01-23 17:28:20 +0700

    [ssss-nodewrap]  update load gmp lib

--- src/js/load-extend-lib.js
+++ src/js/load-extend-lib.js
@@ -12,11 +12,11 @@ if (ffi.LIB_EXT === '.dll') {
 
 // Load boost library and libtorrent library
 if (ffi.LIB_EXT === '.so') {
-  ffi.DynamicLibrary(root_dir + 'lib/linux/libgmp.10' + ffi.LIB_EXT);
-  ffi.DynamicLibrary(root_dir + 'lib/linux/libgmp' + ffi.LIB_EXT);
+  ffi.DynamicLibrary(root_dir + 'lib/linux/libgmp.so.10');
+  ffi.DynamicLibrary(root_dir + 'lib/linux/libgmp.so');
 } if (ffi.LIB_EXT === '.dylib') {
-  ffi.DynamicLibrary(root_dir + 'lib/osx/libgmp.10' + ffi.LIB_EXT);
-  ffi.DynamicLibrary(root_dir + 'lib/osx/libgmp' + ffi.LIB_EXT);
+  ffi.DynamicLibrary(root_dir + 'lib/osx/libgmp.10.dylib');
+  ffi.DynamicLibrary(root_dir + 'lib/osx/libgmp.dylib');
 } else {
-  //
-}	
+  // window
+}
