6dc39207cce1cbb41fcdaee4216f5304dd0f5cf2
bachlx-bitmark <bachlx@bitmark.com>
2017-01-24 10:40:18 +0700

    [ssss-nodewrap]  update link library for osX and  up to v0.0.5

--- scripts/check_installation.sh
+++ scripts/check_installation.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 echo $OSTYPE
 if [[ $OSTYPE == darwin* ]]; then
-	if [[ $(which gcc) ]]; then
-		echo "Compiling tweetnacl library!!"
-		gcc -dynamiclib -undefined suppress -flat_namespace src/c/ssss_custom.c -o src/c/ssss_custom.dylib -lgmp
-	else
-		echo "This machine does not install G++"
-	fi
-fi
-if [[ $OSTYPE == linux-gnu ]]; then
-	if [[ $(dpkg -l | grep g++) ]]; then
-		echo "Compiling tweetnacl library!!"
-		gcc -O2 -shared -fpic src/c/ssss_custom.c -o src/c/ssss_custom.so -lgmp
-	else
-		echo "This machine does not install G++"
-	fi
+	echo "link library for macOS"
+	cd src/c
+	install_name_tool -change libgmp.10.dylib @loader_path/../../lib/osx/libgmp.10.dylib shamir.dylib
 fi
--- scripts/install_dependencies.js
+++ scripts/install_dependencies.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 if (process.platform !== "win32") {
 	console.log("run install for MAC or Linux OS ==================");
 	function puts(error, stdout, stderr) {
-		sys.puts(stdout) 
+		sys.puts(stdout);
 	}
 	exec("./check_installation.sh", puts);
 }
